edb1af5f5cdd6574909e4e756debce5030a1fe84
Rick Belson <belsonrj@aol.com>
2020-05-17 05:24:27 +0000

    --styles

--- concert-tracker/app/models/performer_venue.rb
+++ concert-tracker/app/models/performer_venue.rb
@@ -1,8 +1,4 @@
 class PerformerVenue < ActiveRecord::Base 
-#  attr_accessor :performer_id, :venue_id
   belongs_to :performer
   belongs_to :venue
-#  has_many :performer_id
-#  has_many :venue_id
-
 end
